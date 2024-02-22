d57a6bc74e219b731f24310e1322728760d84df0
srikrishnanaik <39154477+srikrishnanaik@users.noreply.github.com>
2024-02-22 13:32:47 +0530

    Create bootstrap.tf

---
+++ bootstrap.tf
@@ -0,0 +1,23 @@
+
+resource "aws_instance" "openshift_bootstrap_instance" {
+  ami                    = "ami-04e29ab892209d108"
+  instance_type          = "t2.xlarge"
+  count                  = 1
+  subnet_id              = "<Your Subnet ID>"
+  key_name               = "openshift_key"
+  security_group_ids     = ["${var.security_group_id}"] // specify your security group ID here
+  iam_instance_profile   = "openshift"
+  user_data              = file("path/to/bootstrap_userdata.txt") // specify the path to your userdata file
+  tags = {
+    "kubernetes.io/cluster/<your_infra_id_from_metadata.json>" = "shared"
+    "Name"                                                    = "bootstrap"
+  }
+
+  root_block_device {
+    volume_size = 100 // specify the size of the root volume here
+    volume_type = "gp2" // specify the volume type here
+  }
+
+  // Additional block device mappings can be added here
+  // block_device_mappings = file("path/to/mapping.json")
+}
